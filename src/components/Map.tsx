26f526305abcb04245c3edea973ccaf6ab721163
Gregory-Alexis <gregoryalexiss@gmail.com>
2025-02-25 18:33:44 +0100

    Map component tha display the geolocation of the user

---
+++ src/components/Map.tsx
@@ -0,0 +1,63 @@
+import { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { Icon } from 'leaflet';
+import markerIconPng from 'leaflet/dist/images/marker-icon.png';
+import { useGeolocation } from '../hook/useGeolocalisation';
+
+const RecenterAutomatically = ({ lat, lon }: { lat: number; lon: number }) => {
+  const map = useMap();
+  useEffect(() => {
+    map.setView([lat, lon]);
+  }, [lat, lon, map]);
+  return null;
+};
+
+const Map = ({ places }: { places: any[] }) => {
+  const { position } = useGeolocation();
+  const defaultPosition: [number, number] = [48.8566, 2.3522];
+
+  return (
+    <MapContainer center={defaultPosition} zoom={13} style={{ height: '80vh', width: '100%' }}>
+      <TileLayer
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        attribution='&copy; OpenStreetMap contributors'
+      />
+
+      {position && (
+        <>
+          <RecenterAutomatically lat={position.lat} lon={position.lon} />
+          <Marker
+            position={[position.lat, position.lon]}
+            icon={
+              new Icon({
+                iconUrl: markerIconPng,
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+              })
+            }
+          >
+            <Popup>Votre position actuelle 📍</Popup>
+          </Marker>
+        </>
+      )}
+
+      {places.map((place) => (
+        <Marker
+          key={place.id}
+          position={[place.lat, place.lon]}
+          icon={
+            new Icon({
+              iconUrl: markerIconPng,
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+            })
+          }
+        >
+          <Popup>{place.display_name}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
+
+export default Map;
